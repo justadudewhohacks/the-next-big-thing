15d91194a982cf95c3ffee7c649efc00269fc95f
vincent <muehler.v@gmail.com>
2017-12-19 15:26:25 +0100

    adjust ga tracking id

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # the-next-big-thing
 Just the opencv4nodejs webpage build with next.js.
+https://opencv4nodejs.herokuapp.com/
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,13 +14,13 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109709749-1"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109709749-2"></script>
           <script dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'UA-109709749-1');
+              gtag('config', 'UA-109709749-2');
             `
           }} />
           <title>My page</title>
